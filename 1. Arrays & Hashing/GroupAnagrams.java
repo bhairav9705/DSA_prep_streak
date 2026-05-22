133c528a9b00eafe1e3282670b920abb1a99292f
Bhairav Shakthi <bhairav9705@gmail.com>
2026-05-22 15:17:51 +0530

    2 medium problems - 21st May 2026

---
+++ 1. Arrays & Hashing/GroupAnagrams.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+  public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for(String word : strs) {
+            char[] words = word.toCharArray();
+            Arrays.sort(words);
+            String sortedWord = new String(words);
+
+            if(!map.containsKey(sortedWord)) map.put(sortedWord, new ArrayList<>());
+            map.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
